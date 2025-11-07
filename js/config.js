fab17123fa6b132dd99038b58893900305b5ec57
borbix-hubb <borbixz@gmail.com>
2025-11-07 11:22:56 +0700

    Update Google Sheet URL

--- js/config.js
+++ js/config.js
@@ -2,14 +2,14 @@
 // ตั้งค่า Google Spreadsheet URL
 // คัดลอก URL จาก Google Sheets ของคุณมาใส่ตรงนี้
 
-const GOOGLE_SHEET_URL = 'YOUR_GOOGLE_SHEET_URL_HERE';
+const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1iF9q5CyLjPD1yLP4rYnboBKIyPoYhYnFbIkMIT6Oe6w/edit?gid=0#gid=0';
 
 // ตัวอย่าง:
 // const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1ABC123xyz.../edit';
 
 // ==================== Function ====================
 function openGoogleSheet() {
-    if (GOOGLE_SHEET_URL === 'YOUR_GOOGLE_SHEET_URL_HERE') {
+    if (GOOGLE_SHEET_URL === 'https://docs.google.com/spreadsheets/d/1iF9q5CyLjPD1yLP4rYnboBKIyPoYhYnFbIkMIT6Oe6w/edit?gid=0#gid=0') {
         alert('⚠️ กรุณาตั้งค่า Google Sheet URL ในไฟล์ js/config.js');
         return;
     }
